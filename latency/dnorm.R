26529157ae95a8e727870340fe5f9e1889ec1b3e
Tien <thuytien140894@gmail.com>
2018-03-11 00:48:20 -0800

    Prints out the average latency

--- latency/dnorm.R
+++ latency/dnorm.R
@@ -1,6 +1,7 @@
 data <- read.table("record.txt")
 png(file = "dnorm-latency.png")
 x <- data$V1
+print(mean(data$V1))
 y <- dnorm(x, mean = mean(data$V1), sd = sd(data$V1))
 plot(x,y,main="Latency Normal Distribution",xlab="Response time (microsecond)",ylab="Probability")
 dev.off()
